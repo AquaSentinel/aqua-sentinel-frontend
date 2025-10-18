2a1087faeb08a30452092251069b9dc05696947b
Rangaraju Viishhnu <viishhnu2005@gmail.com>
2025-10-19 01:58:15 +0530

    Updated vite configs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    host: "0.0.0.0",   // allows access from external devices
+    port: 3000,        // (optional) choose any port you want
+  },
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 })
