f263235a102db0863f89be3f1835de01426514b8
Rangaraju Viishhnu <viishhnu2005@gmail.com>
2025-10-19 02:12:19 +0530

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 import HomePage from "./pages/HomePage";
 import ProfilePage from "./components/Profile";
-import ShipDetectionPage from "./pages/ShipDetectionPage";
-import MarineDebrisDetectionPage from "./pages/MarineDebrisDetectionPage";
 import DetectionStudio from "./pages/DetectionStudio";
 function App() {
   return (
@@ -15,8 +13,6 @@ function App() {
         <Route path="/signup" element={<SignupPage />}></Route>
         <Route path="/home" element={<DetectionStudio />}/>
           <Route path="/home/profile" element={<ProfilePage />} />
-          <Route path="/home/ship-detection" element={<ShipDetectionPage />} />
-          <Route path="/home/marine-debris-detection" element={<MarineDebrisDetectionPage />} />
       </Routes>
     </>
   );
